2503e55ae266a0a0300a1cd1375ada3b33a7a31b
Nolfgk <pitsbourg@yandex.ru>
2016-11-20 17:32:43 +0300

    setup task

--- src/main/java/com/spoloborota/teaching/storage/main/Main.java
+++ src/main/java/com/spoloborota/teaching/storage/main/Main.java
@@ -2,12 +2,16 @@ package com.spoloborota.teaching.storage.main;
 
 import com.spoloborota.teaching.storage.model.RAM;
 import com.spoloborota.teaching.storage.processor.Processor;
-import com.spoloborota.teaching.storage.view.Console;;
+import com.spoloborota.teaching.storage.view.Console;
 
-/**
- * Main class
- * @author Spoloborota
- *
+/*
+ TO DO
+ Задача по доработке проекта Storage:
+ Реализовать вывод на экран всех элементов выбранного ранее (с использованием команды USE) хранилища. Использовать для этого команду "list".
+ Выполнить задачу в отдельной ветке и прислать мне pull-request. Как делать pull-requst и что это, вообще, такое разобраться самостоятельно.
+ Форкать отсюда:
+ https://github.com/Spoloborota/Storage
+ Выполнить до 28го ноября.
  */
 public class Main {
 
@@ -15,7 +19,8 @@ public class Main {
 		RAM ram = new RAM();
 		Processor processor = new Processor(ram);
 		Console console = new Console(processor);
-		console.startListen();
+        System.out.println("At your service, master.");
+        console.startListen();
 	}
 
 }
