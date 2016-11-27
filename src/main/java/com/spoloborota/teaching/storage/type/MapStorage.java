1625cd7abb7b31a65341edf35085ad37fa46f99f
ShebalovDenis <chempikd@yandex.ru>
2016-11-27 23:50:12 +0300

    Добавление метода

--- src/main/java/com/spoloborota/teaching/storage/type/MapStorage.java
+++ src/main/java/com/spoloborota/teaching/storage/type/MapStorage.java
@@ -1,6 +1,8 @@
 package com.spoloborota.teaching.storage.type;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MapStorage {
 
@@ -15,5 +17,17 @@ public class MapStorage {
 	public boolean add(String[] keyValue) {
 		hashMap.put(keyValue[0], keyValue[1]);
 		return true;
+	}
+	
+	public ArrayList<String> print() {
+		String a = "";
+		ArrayList<String> arr = new ArrayList<String>();
+		for (Map.Entry<String, String> pair : hashMap.entrySet()){
+		 String key = pair.getKey();
+		 String value = pair.getValue();
+		 a = key + " " + value;
+		 arr.add(0, a);
+		}
+		return arr;
 	}
 }
