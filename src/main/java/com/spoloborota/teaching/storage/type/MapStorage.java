70dfc25eb4877de8d761a2f87231ae74be7e98d5
Vadim <wadim-ka95@mail.ru>
2016-12-04 20:06:12 +0300

    Сохранение

--- src/main/java/com/spoloborota/teaching/storage/type/MapStorage.java
+++ src/main/java/com/spoloborota/teaching/storage/type/MapStorage.java
@@ -2,24 +2,35 @@ package com.spoloborota.teaching.storage.type;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MapStorage {
 
 	public String name;
 	public HashMap<String, String> hashMap;
-	
+
 	public MapStorage(String name) {
 		this.name = name;
 		hashMap = new HashMap<>();
 	}
-	
+
 	public boolean add(String[] keyValue) {
 		hashMap.put(keyValue[0], keyValue[1]);
 		return true;
 	}
 
 	public ArrayList<String> save() {
-		// TODO Auto-generated method stub
-		return null;
+		String a = "";
+		String b = "";
+		ArrayList<String> arr = new ArrayList<String>();
+		for (Map.Entry<String, String> pair : hashMap.entrySet()){
+			String key = pair.getKey();
+			String value = pair.getValue();
+			a = key;
+			arr.add(0, a);
+			b = value; 
+			arr.add(0, b);
+		}
+		return arr;
 	}
 }
