65014d4549e94063be7d596ea389459743508215
Vadim <wadim-ka95@mail.ru>
2016-12-04 19:55:51 +0300

    1)чтение ключей и значений из файла. Функция,работающая в JAVA 7 и выше. 2)метод для проверки файлов с расширением storage в указанной папке (директории)

--- src/main/java/com/spoloborota/teaching/storage/view/Console.java
+++ src/main/java/com/spoloborota/teaching/storage/view/Console.java
@@ -1,8 +1,12 @@
 package com.spoloborota.teaching.storage.view;
 
-import java.io.BufferedReader;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import com.spoloborota.teaching.storage.processor.Processor;
 import com.spoloborota.teaching.storage.reader.SingletonReader;
@@ -50,5 +54,29 @@ public class Console {
 	{
 		return url;
 	}
+	//чтение ключей и значений из файла. Функция,работающая в JAVA 7 и выше
+	private List<String> readfile(String url) throws IOException {
+		List<String> lines = Files.readAllLines(Paths.get(url), StandardCharsets.UTF_8);
+		return lines;
+	}
+	// проверяем файлы с расширением storage в указанной папке
+	private String[] check(String url2) {
+		File folder = new File(url2);
 
+		String[] files = folder.list(new FilenameFilter() {
+
+			@Override public boolean accept(File folder, String name) {
+				return name.endsWith(".storage");
+			}
+
+		});
+		String[] files2 = new String[files.length];
+		for (int i = 0; i < files.length; i++){
+			files2[i] = files[i].substring(0, files[i].length()-8);
+		}
+		//		for ( String fileName : files ) {
+		//			System.out.println("File in directory: " + fileName);
+		//		}
+		return files2;
+	}
 }
