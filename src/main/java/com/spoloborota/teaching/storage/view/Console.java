abdd6be15f086095374b58d9f3018c209cc8f171
ShebalovDenis <chempikd@yandex.ru>
2016-12-04 20:02:30 +0300

    что-то

--- src/main/java/com/spoloborota/teaching/storage/view/Console.java
+++ src/main/java/com/spoloborota/teaching/storage/view/Console.java
@@ -14,29 +14,33 @@ import com.spoloborota.teaching.storage.reader.SingletonReader;
 public class Console {
 	public Processor processor;
 	public SingletonReader rdr = SingletonReader.getInstance(); 
-	public String path = " ";
+	public String path = "";
 
 	public Console(Processor processor) {
 		this.processor = processor;
 	}
+	
+	
 
 	public void startListen() {
 		while(true) {
 			try {
 				
-
 				String commandString = rdr.readLine();
 				if(commandString.contains("\\")){
-					path = commandString;
+					path = commandString + "\\save.txt";
 					System.out.println("Your path is " + path);
 				}
 				else{
-					String result = processor.process(commandString);
+					String result = processor.process(commandString);// вот тут если что уберешь
 					System.out.println(result);
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	public String getPath(){
+		return path;
 	}
 }
