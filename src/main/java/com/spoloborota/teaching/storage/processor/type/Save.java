bb18a7a1044a0e1c41eb3465b707e8f822224da9
ShebalovDenis <chempikd@yandex.ru>
2016-12-06 12:51:12 +0300

    Чуть доработал второй пункт

--- src/main/java/com/spoloborota/teaching/storage/processor/type/Save.java
+++ src/main/java/com/spoloborota/teaching/storage/processor/type/Save.java
@@ -6,14 +6,11 @@ import java.io.IOException;
 import com.spoloborota.teaching.storage.model.RAM;
 import com.spoloborota.teaching.storage.processor.Processor;
 import com.spoloborota.teaching.storage.view.Console;
+import com.spoloborota.teaching.storage.type.*;
 //import com.spoloborota.teaching.storage.view.*;
 
 public class Save {
 	
-	public static void main(String[] args) {
-		
-		
-	} 
 		
 	public static String process(RAM ram) {	
 		String isSaved = ram.Save();
@@ -21,7 +18,9 @@ public class Save {
 		Processor p = new Processor(ram);
 		Console c = new Console(p);
 		
-		try(FileWriter writer = new FileWriter(c.path, false))
+		
+		
+		try(FileWriter writer = new FileWriter(c.path + "\\" + ram.getName() + ".storage" , false))
 	    {
 	       // запись всей строки
 	        writer.write(isSaved);
