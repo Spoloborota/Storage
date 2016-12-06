7fba95fe6e7c3220e96db6448b39a58a6ddb4741
ShebalovDenis <chempikd@yandex.ru>
2016-12-06 18:16:39 +0300

    начал делать шатдаун

--- src/main/java/com/spoloborota/teaching/storage/processor/type/Shutdown.java
+++ src/main/java/com/spoloborota/teaching/storage/processor/type/Shutdown.java
@@ -1,5 +1,13 @@
 package com.spoloborota.teaching.storage.processor.type;
 
+import com.spoloborota.teaching.storage.model.RAM;
+
 public class Shutdown {
+	public static String process(RAM ram, char a, char b ) {
+		switch
+		
+		
+		return null;
+	}
 
 }
