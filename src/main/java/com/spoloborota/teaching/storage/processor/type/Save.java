226ed7f22458d28fc8dc3ab700fccb072b0c5c33
ShebalovDenis <chempikd@yandex.ru>
2016-12-11 19:48:54 +0300

    some commit

--- src/main/java/com/spoloborota/teaching/storage/processor/type/Save.java
+++ src/main/java/com/spoloborota/teaching/storage/processor/type/Save.java
@@ -2,6 +2,7 @@ package com.spoloborota.teaching.storage.processor.type;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.spoloborota.teaching.storage.model.RAM;
 import com.spoloborota.teaching.storage.processor.Processor;
@@ -11,47 +12,50 @@ import com.spoloborota.teaching.storage.type.*;
 
 
 public class Save {
-	
-		
+
+
 	public static String process(RAM ram) {	
-		String isSaved = ram.Save();
-		
+		ArrayList<String> isSaved = ram.Save();
+
 		Processor p = new Processor(ram);
 		Console c = new Console(p);
-		
-		
-		
+		String nameCs = ram.getName();
+
+
+
+
 		try(FileWriter writer = new FileWriter(c.path + "\\" + ram.getName() + ".storage" , false))
-	    {
-			
-			String[] s = isSaved.split("\\s+");
-			for(int i = 0; i < s.length; i++)
-			{
-				writer.write(s[i]);
-				writer.append("\n");
+		{
+
+
+			for(int i = 0; i <= isSaved.size()-1; i++){
+				writer.write(isSaved.get(i) + "\r\n");
 				writer.flush();
 			}
-			
-			
-			
-			
-			
-//			String[] s = isSaved.split("\\s+");
-//			for(String o : s){
-//				 writer.write(o + "\n");
-//			}
-//			writer.close();
-//			
-				
-	        
-	    }
-	    catch(IOException ex){
-	         
-	        System.out.println(ex.getMessage());
-	    } 
-		return isSaved;
-		
+
+
+
+
+
+
+
+
+			//			String[] s = isSaved.split("\\s+");
+			//			for(String o : s){
+			//				 writer.write(o + "\n");
+			//			}
+			//			writer.close();
+			//			
+
+
+		}
+		catch(IOException ex){
+
+			System.out.println(ex.getMessage());
+		} 
+		return "Saved seccufully";
+
 	}
 
-	
+
 }
